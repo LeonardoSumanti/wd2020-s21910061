36908a066f3b389f04c6e6ac130b9248a268efd1
LeonardoSumanti <s21910061@student.unklab.ac.id>
2020-11-09 23:53:46 +0800

    pertemuan_23

---
+++ pertemuan_23/index.js
@@ -0,0 +1,9 @@
+let berat = prompt("Masukkan Berat =");
+let tinggi = prompt("Masukkan Tinggi =");
+console.log("Berat yang dimasukkan adalah" + berat);
+console.log("Tinggi yang dimasukkan adalah" + tinggi);
+
+let bilangan1=berat;
+let bilangan2=tinggi;
+
+console.log(bilangan1 / bilangan2 * bilangan2);
